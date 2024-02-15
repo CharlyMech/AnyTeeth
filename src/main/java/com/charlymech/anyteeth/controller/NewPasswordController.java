06d7934199dbd535c73af7328188699138576da5
CharlyMech <sanchezreciocarlos99@gmail.cim>
2024-02-15 19:01:35 +0100

    FUNC mostrar/ocultar panel de info y comprobación de contraseña con regex

--- src/main/java/com/charlymech/anyteeth/controller/NewPasswordController.java
+++ src/main/java/com/charlymech/anyteeth/controller/NewPasswordController.java
@@ -3,7 +3,9 @@ package com.charlymech.anyteeth.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import static com.charlymech.anyteeth.App.rb;
@@ -16,7 +18,9 @@ public class NewPasswordController {
 	@FXML
 	private AnchorPane pane;
 	@FXML
-	private Label newPasswordTitle, passwordLabel1, passwordLabel2;
+	private Pane infoPane;
+	@FXML
+	private Label newPasswordTitle, passwordLabel1, passwordLabel2, passwordInfoTitle;
 	@FXML
 	private PasswordField password1PasswordField, password2PasswordField;
 	@FXML
@@ -25,6 +29,10 @@ public class NewPasswordController {
 	private CheckBox showBothPasswordsCheckBox;
 	@FXML
 	private Button setNewPasswordBtn;
+	@FXML
+	public TextArea passwordRulesTextArea;
+	// Variables de clase
+	private final String passwordRegex = "^(?=.*\\d)(?=.*[A-Z])(?=.*[=\\-+*&^%$#@!.]).{8,}$";
 
 	public void showBothPasswords(ActionEvent event) {
 		if(this.showBothPasswordsCheckBox.isSelected()) { // Mostrar las contraseñas
@@ -47,13 +55,13 @@ public class NewPasswordController {
 	}
 
 	public void setNewPassword(ActionEvent event) {
-		// TODO: check password regex
 		if (this.password1PasswordField.getText().trim().isEmpty() || this.password2PasswordField.getText().trim().isEmpty()) { // Caso que alguno de los campos esté vacío
 			showWarningAlert(rb.getString("alertTitle"), rb.getString("newPasswordNotFilledHeader"), rb.getString("newPasswordNotFilledBody"));
 		} else if (!this.password1PasswordField.getText().trim().equals(this.password2PasswordField.getText().trim())) { // Caso no coinciden
 			showWarningAlert(rb.getString("alertTitle"), rb.getString("newPasswordNotEqualsHeader"), rb.getString("newPasswordNotEqualsBody"));
+		} else if (!this.password1PasswordField.getText().trim().matches(this.passwordRegex)) { // No cumple los parámetros requeridos de la contraseña
+			showWarningAlert(rb.getString("alertTitle"), rb.getString("newPasswordBadPasswordHeader"), rb.getString("newPasswordBadPasswordBody"));
 		} else { // Ambas son iguales y no están vacías
-			// TODO -> Asignar la nueva contraseña a los campos de la pantalla de Staff
 			Stage stage = (Stage) this.pane.getScene().getWindow();
 			stage.close();
 			staticPasswordPasswordField.setText(this.password1PasswordField.getText().trim());
@@ -67,5 +75,15 @@ public class NewPasswordController {
 		this.passwordLabel2.setText(rb.getString("newPasswordPassword2"));
 		this.showBothPasswordsCheckBox.setText(rb.getString("newPasswordPasswordShowPasswords"));
 		this.setNewPasswordBtn.setText(rb.getString("newPasswordSetNewPassword"));
+		this.passwordInfoTitle.setText(rb.getString("newPasswordInfoTitle"));
+		this.passwordRulesTextArea.setText(rb.getString("newPasswordInfoTextArea"));
+	}
+
+	public void showPasswordInfo(MouseEvent mouseEvent) {
+		this.infoPane.setVisible(true);
+	}
+
+	public void hidePasswordInfo(MouseEvent mouseEvent) {
+		this.infoPane.setVisible(false);
 	}
 }
