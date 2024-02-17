eedf0b1d0d3f4e273efb9731c489277ca8a07099
CharlyMech <sanchezreciocarlos99@gmail.cim>
2024-02-17 10:16:51 +0100

    REFACTOR añadir email corporativo (getters y setters), un método toString (temporal) y método de inserción de Staff en DB

--- src/main/java/com/charlymech/anyteeth/db/Staff.java
+++ src/main/java/com/charlymech/anyteeth/db/Staff.java
@@ -16,13 +16,15 @@ public class Staff extends Person {
 	// Variables de clase
 	public static final String corporationEmailRegex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@anyteeth.es$";
 	private String staffID;
+	private String corporationEmail;
 	private String password;
 	private Role role;
 
 	// Constructores
-	public Staff(String staffID, String identification, Identification identificationType, String name, String surnames, Gender gender, Date birthDate, String telephoneNumber, String email, String password, String address, int cp, String population, Province province, MaritalStatus maritalStatus, Date registrationDate, Role role) {
+	public Staff(String staffID, String identification, Identification identificationType, String name, String surnames, Gender gender, Date birthDate, String telephoneNumber, String email, String address, int cp, String population, Province province, MaritalStatus maritalStatus, Date registrationDate, String corporationEmail, String password, Role role) {
 		super(identification, identificationType, name, surnames, gender, birthDate, telephoneNumber, email, address, cp, population, province, maritalStatus, registrationDate); // Llamar al constructor de la clase padre
 		this.staffID = staffID;
+		this.corporationEmail = corporationEmail;
 		this.password = password;
 		this.role = role;
 	}
@@ -69,20 +71,28 @@ public class Staff extends Person {
 
 	// GETTERS //
 	public String getStaffID() {
-		return staffID;
+		return this.staffID;
+	}
+
+	public String getCorporationEmail() {
+		return this.corporationEmail;
 	}
 
 	public String getPassword() {
-		return password;
+		return this.password;
 	}
 
 	public Role getRole() {
-		return role;
+		return this.role;
 	}
 
 	// SETTERS //
 	public void setStaffID(String staffID) {
 		this.staffID = staffID;
+	}
+
+	public void setCorporationEmail(String corporationEmail) {
+		this.corporationEmail = corporationEmail;
 	}
 
 	public void setPassword(String password) {
@@ -91,6 +101,38 @@ public class Staff extends Person {
 
 	public void setRole(Role role) {
 		this.role = role;
+	}
+
+	// TO STRING //
+	@Override
+	public String toString() {
+		return "Staff{" +
+				"staffID='" + staffID + '\'' +
+				", corporationEmail='" + corporationEmail + '\'' +
+				", password='" + password + '\'' +
+				", role=" + role +
+				", identification='" + identification + '\'' +
+				", identificationType=" + identificationType +
+				", name='" + name + '\'' +
+				", surnames='" + surnames + '\'' +
+				", gender=" + gender +
+				", birthDate=" + birthDate +
+				", telephoneNumber='" + telephoneNumber + '\'' +
+				", email='" + email + '\'' +
+				", address='" + address + '\'' +
+				", cp=" + cp +
+				", population='" + population + '\'' +
+				", province=" + province +
+				", maritalStatus=" + maritalStatus +
+				", registrationDate=" + registrationDate +
+				", comments=" + comments +
+				", active=" + active +
+				'}';
+	}
+
+	// Métodos CRUD //
+	public static void insertStaff(Staff createStaff) {
+		System.out.println(createStaff.toString());
 	}
 
 	// Clase enumerada privada para delimitar los roles de los empleados en la aplicación
